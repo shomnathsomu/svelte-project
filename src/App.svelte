71f8a22270e7ee5aec90e83fa204022424a28b60
LinKin-22 <shomnathcse22@gmail.com>
2019-09-11 23:53:42 +0600

    initialized the svelte app files

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../node_modules/materialize-css/dist/css/materialize.min.css';
+	import '../node_modules/materialize-css/dist/js/materialize.min.js';
+
+</script>
+
+<style>
+	
+</style>
+
+<h1>Hello {name}!</h1>
