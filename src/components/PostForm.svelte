a5ed57c38d7ca98d1e6de2a3099e32d3a175e72c
LinKin-22 <shomnathcse22@gmail.com>
2019-09-14 15:50:58 +0600

    created PostForm

---
+++ src/components/PostForm.svelte
@@ -0,0 +1,38 @@
+<script>
+    let title = '';
+    let body = '';
+
+    const apiBaseUrl = "https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev";
+
+    async function onSubmit(event){
+        event.preventDefault();
+
+        if(title.trim() === '' || body.trim() === '') return;
+
+        const newPost = { title, body };
+
+        const res = await fetch(`${apiBaseUrl}/post`, {
+            method: 'POST',
+            body: JSON.stringify(newPost)
+        });
+
+        const post = await res.json();
+    }
+
+</script>
+
+<style>
+
+</style>
+
+<form on:submit = {onSubmit}>
+    <div class="input-field">
+        <label for="title">Title</label>
+        <input type="text" bind:value={title} />
+    </div>
+    <div class="input-field">
+        <label for="body">Body</label>
+        <input type="text" bind:value={body} />
+    </div>
+    <button type="submit" class="waves-effect waves-light btn">Add</button>
+</form>
