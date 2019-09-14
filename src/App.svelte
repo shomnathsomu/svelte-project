543303405c31767a3bd9ed1318ad6665c1aa9075
LinKin-22 <shomnathcse22@gmail.com>
2019-09-14 15:48:20 +0600

    imported home, about and navbar

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,21 @@
 	import '../node_modules/materialize-css/dist/css/materialize.min.css';
 	import '../node_modules/materialize-css/dist/js/materialize.min.js';
 
+	import { Router, Route }  from "svelte-routing";
+
+	import Navbar from './layout/Navbar.svelte';
+	import Home from './pages/Home.svelte';
+	import About from './pages/About.svelte';
 </script>
 
 <style>
 	
 </style>
 
-<h1>Hello {name}!</h1>
+<Router>
+	<Navbar />
+	<div class="container">
+		<Route path="/" component={Home} />
+		<Route path="/about" component={About} />
+	</div>
+</Router>
