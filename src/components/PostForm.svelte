410f70cab81ef5b04c2fad1f37e8fcda1c1d6d0f
LinKin-22 <shomnathcse22@gmail.com>
2019-09-17 01:11:07 +0600

    Finsihed app - routing + CRUD API consumption

--- src/components/PostForm.svelte
+++ src/components/PostForm.svelte
@@ -1,38 +1,73 @@
 <script>
-    let title = '';
-    let body = '';
+  import { createEventDispatcher } from "svelte";
+  export let editingPost;
 
-    const apiBaseUrl = "https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev";
+  const dispatch = createEventDispatcher();
 
-    async function onSubmit(event){
-        event.preventDefault();
+  $: title = editingPost.title;
+  $: body = editingPost.body;
+  let loading = false;
 
-        if(title.trim() === '' || body.trim() === '') return;
+  const apiBaseUrl =
+    "https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev";
 
-        const newPost = { title, body };
+  async function onSubmit(event) {
+    event.preventDefault();
 
-        const res = await fetch(`${apiBaseUrl}/post`, {
-            method: 'POST',
-            body: JSON.stringify(newPost)
-        });
+    if (title.trim() === "" || body.trim() === "") return;
 
-        const post = await res.json();
+    loading = true;
+
+    const newPost = { title, body };
+
+    let url, method;
+
+    if(editingPost.id) {
+        url = `${apiBaseUrl}/post/${editingPost.id}`;
+        method = 'PUT';
+    }
+    else{
+        url = `${apiBaseUrl}/post`;
+        method = 'POST';
     }
 
+    const res = await fetch(url, {
+      method,
+      body: JSON.stringify(newPost)
+    });
+
+    const post = await res.json();
+    dispatch('postCreated', post);
+
+    loading = false;
+  }
 </script>
 
 <style>
-
+  form {
+    margin: 50px;
+  }
+  .progress {
+    margin: 100px 0;
+  }
 </style>
 
-<form on:submit = {onSubmit}>
+{#if !loading}
+  <form on:submit={onSubmit}>
     <div class="input-field">
-        <label for="title">Title</label>
-        <input type="text" bind:value={title} />
+      <label for="title">Title</label>
+      <input type="text" bind:value={editingPost.title} />
     </div>
     <div class="input-field">
-        <label for="body">Body</label>
-        <input type="text" bind:value={body} />
+      <label for="body">Body</label>
+      <input type="text" bind:value={editingPost.body} />
     </div>
-    <button type="submit" class="waves-effect waves-light btn">Add</button>
-</form>
+    <button type="submit" class="waves-effect waves-light btn">
+        {editingPost.id ? "UPDATE" : "ADD"}
+    </button>
+  </form>
+{:else}
+  <div class="progress">
+    <div class="indeterminate" />
+  </div>
+{/if}
